437466f858cc3a97070d8bc449143b142e30cefe
Nivek <54602828+kyeung7@users.noreply.github.com>
2021-01-11 19:15:59 -0500

    more library installls

--- notebook.py
+++ notebook.py
@@ -1,15 +1,24 @@
+######### 1
 # import quandl
 # import pandas as pd
 # quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
 # df = quandl.get("WIKI/AAPL") #choose stock with linear pattern
 # df.head()
 
+######### 2
+# import quandl
+# import pandas
+# quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
+# df = quandl.get("WIKI/AAPL") #choose stock with linear pattern
+# df = df[['Adj. CLose']]
+# df.head()
 
+########## 3
 import quandl
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
-
-df = quandl.get("WIKI/AAPL") #choose stock with linear pattern
-df = df[['Adj. CLose']]
-df.head()
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
