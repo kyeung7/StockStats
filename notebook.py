ee2fb65376b950ca0e070b95e6673d11519ccfbf
Nivek <54602828+kyeung7@users.noreply.github.com>
2021-01-11 19:06:19 -0500

    testing quandl functions

--- notebook.py
+++ notebook.py
@@ -1,2 +1,15 @@
+# import quandl
+# import pandas as pd
+# quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
+# df = quandl.get("WIKI/AAPL") #choose stock with linear pattern
+# df.head()
+
+
 import quandl
 import pandas as pd
+
+quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
+
+df = quandl.get("WIKI/AAPL") #choose stock with linear pattern
+df = df[['Adj. CLose']]
+df.head()
