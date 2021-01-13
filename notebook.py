0c9b6e8a0da8dcab7279505011d6e7717a3cff40
Nivek <54602828+kyeung7@users.noreply.github.com>
2021-01-12 21:21:31 -0500

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,24 +1,20 @@
-######### 1
-# import quandl
-# import pandas as pd
-# quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
-# df = quandl.get("WIKI/AAPL") #choose stock with linear pattern
-# df.head()
-
-######### 2
-# import quandl
-# import pandas
-# quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
-# df = quandl.get("WIKI/AAPL") #choose stock with linear pattern
-# df = df[['Adj. CLose']]
-# df.head()
-
-########## 3
+# Imports used
 import quandl
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.linear_model import LinearRegression
+
+# API setup
+quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
+df = quandl.get("WIKI/AAPL") #choose stock with linear pattern
+df.head()
+ 
+df = df[['Adj. CLose']]
+df.head()
+
+df['Adj. Close'].plot(figsize=(15, 6), color='g')
+plt.legend(loc='upper left')
+plt.show()
