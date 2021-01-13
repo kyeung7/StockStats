bd7a21c80d33610e9bb601abd47b7ac30c750c73
Nivek <54602828+kyeung7@users.noreply.github.com>
2021-01-12 21:33:54 -0500

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,13 +1,24 @@
-#Part 1 - print dataset
-import quandl
-import pandas as pd
+# #Part 1 - print dataset
+# import quandl
+# import pandas as pd
 
-quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
+# quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
 
-df = quandl.get("WIKI/AAPL")
-df.head()
+# df = quandl.get("WIKI/AAPL")
+# df.head()
 
-#Part 1 - print dataset
+# #Part 2 - view adjusted close coloumn for analysis
+# import quandl
+# import pandas as pd
+
+# quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
+
+# df = quandl.get("WIKI/AAPL")
+# df= df[["Adj. Close"]]
+
+# df.head()
+
+#Part 3 - show graph of stock price data over time
 # Imports used
 import quandl
 import pandas as pd
@@ -22,12 +33,9 @@ quandl.ApiConfig.api_key = 'LsYwP9quyK2ezGbiNKGq'
 
 # choosing stock with linear pattern and showing data
 df = quandl.get("WIKI/AAPL")
-df.head()
- 
-# SHOW APPL
-df = df['Adj. CLose']
+df= df[["Adj. Close"]]
 df.head()
 
-df['Adj. Close'].plot(figsize=(15, 6), color='g')
-plt.legend(loc='upper left')
+df["Adj. Close"].plot(figsize = (15, 6), color = "g")
+plt.legend(loc = "upper left")
 plt.show()
